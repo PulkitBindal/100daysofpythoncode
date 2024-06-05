4cd12ddd84431b440362b0ef4e72e2e48dddab87
Pulkit Bindal <pulkit507shamli@gmail.com>
2024-06-05 17:41:30 +0530

    Adding Python Program 2

---
+++ program2.py
@@ -0,0 +1,26 @@
+# Declaration of List and Usage
+l = []
+l.append(4)
+l.append(10)
+print(l)
+print(type(l))
+#Declaration of Dictionary and usage 
+d={}
+d[4]="Four"
+d[10]="Ten"
+print(d)
+print(type(d))
+#Declaration of Set and Usage
+s=set()
+s.add(1)
+s.add(3)
+print(s)
+print(type(s))
+# Creating list via for loop
+l=[i for i in range(1,10)]
+print(l)
+print(type(l))
+# Declaration of Tuple and Usage
+t=tuple(l)
+print(t)
+print(type(t))
